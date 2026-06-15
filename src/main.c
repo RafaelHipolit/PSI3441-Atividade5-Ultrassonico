0a4f380b04ad26877be245190f44605351454579
Rafael Hipólito <rafaelhipolitorodrigues@gmail.com>
2026-06-15 01:46:35 -0300

    lib hcsr funcionando

--- src/main.c
+++ src/main.c
@@ -3,24 +3,24 @@
 #include "pwm_z42.h"
 #include "hcsr04.h"
 
-// TODO: colocar "hcsr04.h" na pasta lib e fazer funcionar
+// TODO: colocar uns comentarios sobre funcinamento
 
 void main(void)
 {
     printk("Sistema Inicializado. Lendo Sensor HC-SR04...\n");
 
-    // Inicializa o hardware do sensor com uma única chamada
+    // Inicializa o hardware do sensor com uma unica chamada
     hcsr04_init();
 
     while (1)
     {
-        // Verifica continuamente (sem travar a CPU) se o sensor concluiu uma medição
+        // Verifica continuamente (sem travar a CPU) se o sensor concluiu uma medicao
         if (hcsr04_is_data_ready())
         {
             uint32_t distancia = hcsr04_get_distance_cm();
             printk("Distancia atual: %u cm\n", distancia);
         }
         
-        k_msleep(10); // Pausa leve para não sobrecarregar o loop
+        k_msleep(10); // Pausa leve para nao sobrecarregar o loop
     }
 }
